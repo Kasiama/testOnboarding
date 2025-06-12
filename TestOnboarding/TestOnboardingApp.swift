a642bf86fa9452bf3645985b428959acdf775441
Ivan Zhuravskiy <i.zhurauski@aiby.com>
2025-06-12 18:59:53 +0300

    Initial Commit

---
+++ TestOnboarding/TestOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOnboardingApp.swift
+//  TestOnboarding
+//
+//  Created by Ivan Zhuravskiy on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TestOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
